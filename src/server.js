59f8299508f0121f054208b9d94a8f69440b63a1
DESKTOP-SHCHOI\iamchoi <thankful.t.u@gmail.com>
2022-04-18 22:19:34 +0900

    babel, pug, express setting

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+// view
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views")
+
+// static
+app.use('/public', express.static(__dirname + '/public'));
+
+// router
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen)

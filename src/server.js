14356214106b7f93673624ffa5084dd6c60d0592
Choi-soohyun <thankful.t.u@gmail.com>
2022-04-19 08:46:11 +0900

    router another path

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 // view
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views")
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views')
 
 // static
 app.use('/public', express.static(__dirname + '/public'));
 
 // router
-app.get("/", (req, res) => res.render("home"));
+app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen)

2e6a25f676a9e395d7cfca7b92fa38253638ac7d
DESKTOP-SHCHOI\iamchoi <thankful.t.u@gmail.com>
2022-04-20 01:00:25 +0900

    Socket IO

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -14,4 +16,34 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen)
+// app.listen(3000, handleListen)
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server }); // [{ server }] http & ws = If you want to run on the same port, WebSocket on the Http 
+
+function onSocketClose() {
+   console.log('Disconnected from the Browser')
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+   sockets.push(socket)
+   socket['nickname'] = 'Anon'
+   console.log('Connected to Browser');
+   socket.on('close', onSocketClose);
+   socket.on('message', (msg) => {
+      const message = JSON.parse(msg.toString('utf8'))
+
+      if(message.type === 'new_message') {
+         sockets.forEach(aSocket => {
+            aSocket.send(`${socket.nickname}: ${message.payload}`)
+         });
+      } else if(message.type === 'nickname') {
+         console.log(message.payload);
+         socket['nickname'] = message.payload;
+      }
+   });
+})
+
+server.listen(3000, handleListen);
